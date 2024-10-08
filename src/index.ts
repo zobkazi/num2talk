3a4c8f9ec6d64479d6b82ff7319689b595303454
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-10-08 13:08:08 +0600

    v1.0.0 releases

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import num2Word from './languages/num2Word';
 import num2Money from './languages/num2bn';
-const num = 100098
 
-console.log(num2Word(num))
-
-// নয়  হাজার নয় শত নব্বই  টাকা 
-console.log(num2Money(num))
+export { num2Word, num2Money };
---
+++ src/test/index.js
@@ -0,0 +1,5 @@
+const {num2Money, num2Word} = require('../index')
+const num  = 102
+
+console.log(num2Money(num))
+console.log(num2Word(num))
